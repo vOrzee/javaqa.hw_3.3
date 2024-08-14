b3080cc7df21f6fbf7e677dc7a933e68b488c6e3
Roman Leshin <vorzee@gmail.com>
2024-08-14 17:21:35 +0500

    Разместили в классе Main код согласно условию, который будем запускать.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CreditPaymentService service = new CreditPaymentService();
+        int loanAmountInRubles = 1_000_000; // сумма кредита
+        double interestRateInPercents = 9.99; // процентная ставка
+        int loanTermInYears = 1; // срок кредита 1 год
+
+        int monthlyLoanPayment = CreditPaymentService
+                .calculateMonthlyPayment(
+                        loanAmountInRubles,
+                        interestRateInPercents,
+                        loanTermInYears
+                ); // Должно получиться 87911
+        System.out.println(monthlyLoanPayment);
+
+        loanTermInYears = 2; // срок кредита 2 года
+        monthlyLoanPayment = CreditPaymentService
+                .calculateMonthlyPayment(
+                        loanAmountInRubles,
+                        interestRateInPercents,
+                        loanTermInYears
+                ); // Должно получиться 87911
+        System.out.println(monthlyLoanPayment);
+
+        loanTermInYears = 3; // срок кредита 3 года
+        monthlyLoanPayment = CreditPaymentService
+                .calculateMonthlyPayment(
+                        loanAmountInRubles,
+                        interestRateInPercents,
+                        loanTermInYears
+                ); // Должно получиться 87911
+        System.out.println(monthlyLoanPayment);
     }
 }
