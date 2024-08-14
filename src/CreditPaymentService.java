8cad87e511b803a5147a7503410ab22f27fa0f93
Roman Leshin <vorzee@gmail.com>
2024-08-14 17:30:15 +0500

    Убрали `Math.round` так как про эту команду нам в условии не написали.

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,6 +2,6 @@ public class CreditPaymentService {
     public int calculateMonthlyPayment(int loanAmountInRubles, double interestRateInPercents, int loanTermInYears) {
         double monthlyInterestRate = interestRateInPercents / 12 / 100;
         double monthlyPayment = (loanAmountInRubles * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -loanTermInYears * 12));
-        return (int) Math.round(monthlyPayment);
+        return (int) monthlyPayment;
     }
 }
